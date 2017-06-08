95c309a59645c4cc9df257bf2925b1c25f30201d
Alexandre De Pablo <alexandre.depablo@gmail.com>
2017-06-08 16:57:30 +0200

    Basic implementation of the list of data providers

---
+++ src/components/ControlPanel.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="control-panel">
+    <data-provider v-for="provider in dataProviders" :name="provider.name" :status="provider.status"></data-provider>
+  </div>
+</template>
+
+<script>
+import DataProvider from '@/components/DataProvider'
+
+export default {
+  name: 'control-panel',
+  data () {
+    return {
+      dataProviders: [
+        {
+          name: 'INSEE',
+          status: 'UP'
+        },
+        {
+          name: 'DGFIP',
+          status: 'UP'
+        },
+        {
+          name: 'Infogreffe',
+          status: 'DOWN'
+        },
+        {
+          name: 'URSSAF',
+          status: 'UP'
+        }
+      ]
+    }
+  },
+  components: {
+    'data-provider': DataProvider
+  }
+}
+</script>
---
+++ src/components/DataProvider.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="data-provider">
+    <ul>
+      <li>name : {{ name }}</li>
+      <li>status : {{ status }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'data-provider',
+  data () {
+    return {}
+  },
+  props: ['name', 'status']
+}
+</script>
+
