c174c01e3c80df48fe70cdf0f5251e4a28e76bd5
Alexandre De Pablo <alexandre.depablo@gmail.com>
2017-06-12 12:20:23 +0200

    Adding Endpoint component

--- src/components/ControlPanel.vue
+++ src/components/ControlPanel.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="control-panel">
+  <div id="control-panel">
+    <h2>Fournisseurs de données</h2>
     <data-provider v-for="provider in dataProviders" :name="provider.name" :status="provider.status"></data-provider>
+
+    <h2>Endpoints</h2>
+    <endpoint v-for="endpoint in endpoints" :name="endpoint.name" :status="endpoint.status" :dependencies="endpoint.dependencies"></endpoint>
   </div>
 </template>
 
 <script>
 import DataProvider from '@/components/DataProvider'
+import Endpoint from '@/components/Endpoint'
 
 export default {
   name: 'control-panel',
@@ -28,11 +33,29 @@ export default {
           name: 'URSSAF',
           status: 'UP'
         }
+      ],
+      endpoints: [
+        {
+          name: 'Etablissements',
+          status: 'UP',
+          dependencies: ['INSEE']
+        },
+        {
+          name: 'Exercices',
+          status: 'UP',
+          dependences: ['INSEE', 'Infogreffe']
+        },
+        {
+          name: 'Attestations Fiscales',
+          status: 'DOWN',
+          dependencies: ['DGFIP']
+        }
       ]
     }
   },
   components: {
-    'data-provider': DataProvider
+    'data-provider': DataProvider,
+    'endpoint': Endpoint
   }
 }
 </script>
--- src/components/Endpoint.vue
+++ src/components/Endpoint.vue
@@ -1,19 +1,20 @@
 <template>
-  <div class="data-provider">
+  <div class="endpoint">
     <ul>
       <li>name : {{ name }}</li>
       <li>status : {{ status }}</li>
+      <li>dependencies : {{ dependencies }}</li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'data-provider',
+  name: 'endpoint',
   data () {
     return {}
   },
-  props: ['name', 'status']
+  props: ['name', 'status', 'dependencies']
 }
 </script>
 
